4ab8ffee25566e323808086a08b61ecf20c72a66
Mohd Arman <110353885+MohdArman123@users.noreply.github.com>
2025-11-30 00:51:44 +0530

    Refactor CORS settings to use ALLOWED_ORIGINS

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from routes.contact_routes import contactRouter
-from config import settings
+from config import settings, ALLOWED_ORIGINS
 app = FastAPI()
 
 # Configure CORS to allow requests from frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.CORS_ORIGINS,  
+    allow_origins=ALLOWED_ORIGINS  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -22,4 +22,5 @@ async def root():
     return {"message": "Welcome to E. Alam Fabricator API"}
 
 if __name__ == "__main__":
+
     uvicorn.run("main:app", host="0.0.0.0", port=settings.APP_PORT, log_level="info", reload=True)
